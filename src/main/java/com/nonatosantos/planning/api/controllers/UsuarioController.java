01cc9385a890addad7d65edf6cca0283c485768b
NONATO SANTOS <nonatosantos1986@gmail.com>
2018-12-02 17:04:23 -0300

    Implementando UsuarioDto e UsuarioController

---
+++ src/main/java/com/nonatosantos/planning/api/controllers/UsuarioController.java
@@ -0,0 +1,87 @@
+package com.nonatosantos.planning.api.controllers;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nonatosantos.planning.api.dtos.UsuarioDto;
+import com.nonatosantos.planning.api.model.Usuario;
+import com.nonatosantos.planning.api.response.Response;
+import com.nonatosantos.planning.api.services.UsuarioService;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+	@Autowired
+	private UsuarioService usuarioService;
+
+	public UsuarioController() {
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Response<UsuarioDto>> cadastrar(@Valid @RequestBody UsuarioDto usuarioDto,
+			BindingResult result) throws NoSuchAlgorithmException {
+		Response<UsuarioDto> response = new Response<UsuarioDto>();
+
+		validarUsuario(usuarioDto, result);
+		Usuario usuario = this.converterDtoUsuario(usuarioDto, result);
+
+		if (result.hasErrors()) {
+
+			result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+			return ResponseEntity.badRequest().body(response);
+		}
+
+		this.usuarioService.salvar(usuario);
+
+		response.setData(this.converterCadastroPFDto(usuario));
+		return ResponseEntity.ok(response);
+
+	}
+
+	private void validarUsuario(UsuarioDto usuarioDto, BindingResult result) {
+		this.usuarioService.buscarPorCpf(usuarioDto.getCpf())
+				.ifPresent(func -> result.addError(new ObjectError("usuario", "CPF já existe")));
+
+		this.usuarioService.buscarPorEmail(usuarioDto.getEmail())
+				.ifPresent(func -> result.addError(new ObjectError("usuario", "Email já existe")));
+
+	}
+
+	private Usuario converterDtoUsuario(UsuarioDto usuarioDto, BindingResult result) throws NoSuchAlgorithmException {
+		Usuario usuario = new Usuario();
+		usuario.setNome(usuarioDto.getNome());
+		usuario.setEmail(usuarioDto.getEmail());
+		usuario.setCpf(usuarioDto.getCpf());
+		usuario.setPerfil(usuarioDto.getPerfil());
+		usuario.setSenha(usuarioDto.getSenha());
+		usuario.setCargo(usuarioDto.getCargo());
+
+		return usuario;
+	}
+
+	private UsuarioDto converterCadastroPFDto(Usuario usuario) {
+		UsuarioDto usuarioDto = new UsuarioDto();
+		usuarioDto.setId(usuario.getId());
+		usuarioDto.setNome(usuario.getNome());
+		usuarioDto.setEmail(usuario.getEmail());
+		usuarioDto.setSenha(usuario.getSenha());
+		usuarioDto.setCpf(usuario.getCpf());
+		usuarioDto.setPerfil(usuario.getPerfil());
+		usuarioDto.setCargo(usuario.getCargo());
+
+		return usuarioDto;
+
+	}
+}
--- src/main/java/com/nonatosantos/planning/api/repositories/UsuarioRepository.java
+++ src/main/java/com/nonatosantos/planning/api/repositories/UsuarioRepository.java
@@ -2,10 +2,18 @@ package com.nonatosantos.planning.api.repositories;
 
 import java.util.Optional;
 
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.nonatosantos.planning.api.model.Usuario;
 
+@Transactional(readOnly = true)
+@NamedQueries({
+		@NamedQuery(name = "UsuarioRepository.findByUsuarioId", 
+				query = "SELECT user FROM Usuario user WHERE user.usuario.id = :usuarioId") })
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	
 	Usuario findByEmail(String email);
